68296982ee189153148e5483cb106f941e92c033
mike6776 <kurthym2010@gmail.com>
2026-02-04 18:18:22 +0100

    Függvény_2

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def összeadás(a, b):
+﻿def osszead(a, b):
     return a + b
+
+if __name__ == "__main__":
+        a = float(input("Add meg az első számot: "))
+        b = float(input("Add meg a második számot:"))
+        eredmény = osszead(a, b)
