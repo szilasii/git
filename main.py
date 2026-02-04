9d6ec2b342f0ba08e4b10498f516bff4ed5d3027
Ethereal <etherealonthewifi@gmail.com>
2026-02-04 18:33:28 +0100

    fix wrong

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def minus(a,b):
-    return a - b
+def kivon(a, b):
+    return a + b
 
-print(minus(5,2))
 print(osszead(5,4))
 print(szoroz(3,4))
 print(kivon(4,5))
->>>>>>> 85fbc0728aaa6a998075db1d068ad38b08363c99
